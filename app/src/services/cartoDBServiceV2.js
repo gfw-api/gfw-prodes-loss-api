c66e7ad990d5660edf39d02b9c1d9c02f7778d68
Adam J. Pain <aj.pain89@gmail.com>
2018-10-05 15:38:06 +0200

    Add dynamic simplify

--- app/src/services/cartoDBServiceV2.js
+++ app/src/services/cartoDBServiceV2.js
@@ -19,7 +19,7 @@ const WORLD = `WITH tmp_group AS (SELECT *
 
 const AREA = `select ST_Area(ST_SetSRID(ST_GeomFromGeoJSON('{{{geojson}}}'), 4326), TRUE)/10000 as area_ha`;
 
-const ISO = `with s as (SELECT st_makevalid(st_simplify(the_geom, 0.05)) as the_geom, area_ha
+const ISO = `with s as (SELECT st_makevalid(st_simplify(the_geom, {{simplify}})) as the_geom, area_ha
             FROM gadm36_countries
             WHERE gid_0 = UPPER('{{iso}}'))
 
@@ -31,7 +31,7 @@ const ISO = `with s as (SELECT st_makevalid(st_simplify(the_geom, 0.05)) as the_
               GROUP BY s.area_ha
             `;
 
-const ID1 = ` with s as (SELECT st_makevalid(st_simplify(the_geom, 0.05)) as the_geom, area_ha
+const ID1 = ` with s as (SELECT st_makevalid(st_simplify(the_geom, {{simplify}})) as the_geom, area_ha
             FROM gadm36_adm1
             WHERE gid_0 = UPPER('{{iso}}') AND gid_1 = '{{id1}}')
 
@@ -43,7 +43,7 @@ const ID1 = ` with s as (SELECT st_makevalid(st_simplify(the_geom, 0.05)) as the
              GROUP BY s.area_ha
             `;
 
-const ID2 = ` with s as (SELECT st_makevalid(st_simplify(the_geom, 0.05)) as the_geom, area_ha
+const ID2 = ` with s as (SELECT st_makevalid(st_simplify(the_geom, {{simplify}})) as the_geom, area_ha
             FROM gadm36_adm2
             WHERE gid_0 = UPPER('{{iso}}') AND gid_1 = '{{id1}}' AND gid_2 = '{{id2}}')
 
@@ -118,6 +118,15 @@ let defaultDate = function () {
     return from + ',' + to;
 };
 
+const getSimplify = (iso) => {
+    let thresh = 0.005;
+    if (iso) {
+      const bigCountries = ['USA', 'RUS', 'CAN', 'CHN', 'BRA', 'IDN'];
+      thresh = bigCountries.includes(iso) ? 0.05 : 0.005;
+    }
+    return thresh;
+  };
+
 class CartoDBService {
 
     constructor() {
@@ -146,11 +155,13 @@ class CartoDBService {
     * getAdm0(iso, period = defaultDate()) {
         logger.debug('Obtaining national of iso %s', iso);
         const gid = routeToGid(iso);
+        const simplify = getSimplify(iso);
         let periods = period.split(',');
         let params = {
             iso: gid.adm0,
             begin: periods[0],
-            end: periods[1]
+            end: periods[1],
+            simplify
         };
         let data = yield executeThunk(this.client, ISO, params);
         if (data && data.rows && data.rows.length > 0) {
@@ -167,12 +178,14 @@ class CartoDBService {
     * getAdm1(iso, id1, period = defaultDate()) {
         logger.debug('Obtaining subnational of iso %s and id1', iso, id1);
         const gid = routeToGid(iso, id1);
+        const simplify = getSimplify(iso) / 10;
         let periods = period.split(',');
         let params = {
             iso: gid.adm0,
             id1: gid.adm1,
             begin: periods[0],
-            end: periods[1]
+            end: periods[1],
+            simplify
         };
         let data = yield executeThunk(this.client, ID1, params);
         if (data && data.rows && data.rows.length > 0) {
@@ -189,13 +202,15 @@ class CartoDBService {
     * getAdm2(iso, id1, id2, period = defaultDate()) {
     logger.debug('Obtaining subnational of iso %s and id1', iso, id1);
     const gid = routeToGid(iso, id1, id2);
+    const simplify = getSimplify(iso) / 100;
     let periods = period.split(',');
     let params = {
         iso: gid.adm0,
         id1: gid.adm1,
         id2: gid.adm2,
         begin: periods[0],
-        end: periods[1]
+        end: periods[1],
+        simplify
     };
     let data = yield executeThunk(this.client, ID2, params);
     if (data && data.rows && data.rows.length > 0) {
