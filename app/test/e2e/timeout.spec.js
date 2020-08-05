33702e2b1d00ab0005ae1cc79147f87f99982d9b
Henrique Pacheco <henrique.pacheco@vizzuality.com>
2020-08-05 12:03:14 +0100

    Handle timeout issues with CartoDB responses

--- app/test/e2e/timeout.spec.js
+++ app/test/e2e/timeout.spec.js
@@ -26,8 +26,8 @@ describe('CartoDB timeout handling', () => {
         const response = await requester.get('/api/v1/prodes-loss/admin/BRA?period=2001-01-01%2C2020-12-31&thresh=30');
         response.status.should.equal(504);
         response.body.should.be.an('object').and.have.property('errors');
-        response.body.errors.should.be.an('array').and.have.length(0);
-        response.body.errors[0].should.have.property('details').and.equal('You are over platform\'s limits: SQL query timeout error. Refactor your query before running again. ');
+        response.body.errors.should.be.an('array').and.have.length(1);
+        response.body.errors[0].should.equal('SQL query timeout error. Refactor your query and try running again.');
     });
 
     afterEach(() => {
